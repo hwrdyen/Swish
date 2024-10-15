d710eb64725bcfc9780796e50b0c0d54bb1dcc81
hwrdyen <hwrdpvt@gmail.com>
2024-10-15 21:46:16 +0800

    Initiated profileForm for update-profile section

---
+++ client/src/components/profileForm/ProfileForm.tsx
@@ -0,0 +1,12 @@
+const ProfileForm = () => {
+  return (
+    <div>
+      <form>
+        <input type="text" name="username" placeholder="username" />
+        <button>Update</button>
+      </form>
+    </div>
+  );
+};
+
+export default ProfileForm;

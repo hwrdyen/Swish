2a392ea71bd39885c828e9c64be5eec5ebdbf601
hwrdyen <hwrdpvt@gmail.com>
2024-10-28 12:43:16 +0800

    Drafted CreateGame page

--- client/src/pages/createGame/CreateGame.tsx
+++ client/src/pages/createGame/CreateGame.tsx
@@ -1,7 +1,8 @@
 import { Link } from "react-router-dom";
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import { AuthContext } from "../../context/AuthContext";
 import { useTours } from "../../hooks/useTour";
+import { useTeams } from "../../hooks/useTeam";
 
 const CreateGame = () => {
   const authContext = useContext(AuthContext);
@@ -12,7 +13,23 @@ const CreateGame = () => {
   }
   const { isLoggedIn } = authContext;
 
+  const { allTeamData } = useTeams();
   const { allTourData } = useTours();
+
+  const [homeTeam, setHomeTeam] = useState<string>("");
+  const [awayTeam, setAwayTeam] = useState<string>("");
+
+  const handleHomeTeamSelectChange = (value: string) => {
+    setHomeTeam(value);
+  };
+
+  const handleAwayTeamSelectChange = (value: string) => {
+    setAwayTeam(value);
+  };
+
+  if (!allTeamData || !allTourData) {
+    return <div>Loading...</div>;
+  }
 
   return (
     <div>
@@ -21,13 +38,69 @@ const CreateGame = () => {
           allTourData.length > 0 ? (
             <form>
               <h1>Create A Game</h1>
+
+              <h2>Select Home Team</h2>
+              <div id="hometeam_container">
+                <div
+                  style={{
+                    display: "flex",
+                    alignItems: "center",
+                    marginBottom: "8px",
+                  }}
+                >
+                  <select
+                    value={homeTeam}
+                    onChange={(e) => handleHomeTeamSelectChange(e.target.value)}
+                  >
+                    <option value="" disabled>
+                      Select Home Team
+                    </option>
+                    {allTeamData.map((allTeamSingleData) => (
+                      <option
+                        key={allTeamSingleData.id}
+                        value={allTeamSingleData.id}
+                      >
+                        {allTeamSingleData.team_name}
+                      </option>
+                    ))}
+                  </select>
+                </div>
+              </div>
+
+              <h2>Select Away Team</h2>
+              <div id="awayteam_container">
+                <div
+                  style={{
+                    display: "flex",
+                    alignItems: "center",
+                    marginBottom: "8px",
+                  }}
+                >
+                  <select
+                    value={awayTeam}
+                    onChange={(e) => handleAwayTeamSelectChange(e.target.value)}
+                  >
+                    <option value="" disabled>
+                      Select Away Team
+                    </option>
+                    {allTeamData.map((allTeamSingleData) => (
+                      <option
+                        key={allTeamSingleData.id}
+                        value={allTeamSingleData.id}
+                      >
+                        {allTeamSingleData.team_name}
+                      </option>
+                    ))}
+                  </select>
+                </div>
+              </div>
             </form>
           ) : (
             <Link to={"/create-tour"}>Create Tournament First</Link>
           )
         ) : (
           <div>
-            please login at <Link to={"/login"}>Login</Link>{" "}
+            Please login at <Link to={"/login"}>Login</Link>
           </div>
         )}
       </div>
